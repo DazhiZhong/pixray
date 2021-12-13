8d4381272a49cd23e05fa9944ff041570ca16ca3
Dazhi Zhong <dazhizho@usc.edu>
2021-12-13 03:08:07 -0800

    add to target loss

--- Losses/TargetLoss.py
+++ Losses/TargetLoss.py
@@ -4,10 +4,16 @@ from PIL import Image
 from torch import nn
 from torchvision.transforms import functional as TF
 from Losses.LossInterface import LossInterface
+import lpips
 
 class TargetLoss(LossInterface):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        self.t_im = None
+        self.loss_fn_vgg = lpips.LPIPS(net='vgg')
+        if self.device!="cpu":
+            self.loss_fn_vgg.cuda()
+        
     
     @staticmethod
     def add_settings(parser):
@@ -28,12 +34,19 @@ class TargetLoss(LossInterface):
                 target_im = Image.open(args.true_target_image )
         target_im = TF.to_tensor(target_im)
         target_im = target_im.to(self.device)
+        target_im  = TF.resize(target_im, (args.size[0],args.size[1]),TF.InterpolationMode.BICUBIC)
+        print('resize?')
         lossglobals = {
             "target_im":target_im,
         }
         return lossglobals
 
     def get_loss(self, cur_cutouts, out, args, globals=None, lossGlobals=None):
-        mseloss = nn.MSELoss()
-        cur_loss = mseloss(out, lossGlobals['target_im']) 
+        # mseloss = nn.MSELoss()
+        if self.t_im is None:
+            self.t_im = TF.resize(lossGlobals['target_im'], out.size()[2:4],TF.InterpolationMode.BICUBIC)
+        # cur_loss = mseloss(out, self.t_im) * 0.5
+        cur_loss = torch.sum(self.loss_fn_vgg(out, self.t_im)).to(self.device)
         return cur_loss * args.true_target_weight
+
+
