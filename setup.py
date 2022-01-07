6e1a3a89d89390f8e111477a2912f5a62b54299e
Dazhi Zhong <dazhizho@usc.edu>
2022-01-06 17:15:25 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ class InstallPixray(install):
     def run(self):
         install.run(self)
         # path = os.getcwd().replace(" ", "\ ").replace("(","\(").replace(")","\)") + "/bin/"
-        os.system("git clone https://github.com/pixray/diffvg && cd diffvg && git submodule update --init --recursive && CMAKE_PREFIX_PATH=$(pyenv prefix) DIFFVG_CUDA=1 python setup.py install")
-        os.system("git clone https://github.com/pixray/v-diffusion-pytorch")
+        os.system("git clone https://github.com/pixray/diffvg && cd diffvg && git submodule update --init --recursive && DIFFVG_CUDA=1 python setup.py install")
+        os.system("git clone https://github.com/dazhizhong/v-diffusion-pytorch -b addcolabs")
 
 setup(
     name='pixray',
     version='1.5.1',    
-    description='neural generation engine',
+    description='text-to-image neural generation engine',
     url='https://github.com/dazhizhong/pixray',
     author='Dazhi Zhong, dribnet',
-    packages=['pixray'],
+    packages=['.'],
     install_requires=[],
     classifiers=[],
     cmdclass={'install': InstallPixray},
