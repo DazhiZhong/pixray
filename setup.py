af368f8f64814d444419817234db5c8245f1bc21
Dazhi Zhong <dazhizho@usc.edu>
2022-01-07 02:06:02 -0800

    0

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ class InstallPixray(install):
         # path = os.getcwd().replace(" ", "\ ").replace("(","\(").replace(")","\)") + "/bin/"
         os.system("git clone https://github.com/pixray/diffvg && cd diffvg && git submodule update --init --recursive && DIFFVG_CUDA=1 python setup.py install")
         os.system("git clone https://github.com/dazhizhong/v-diffusion-pytorch")
+        os.system("cp -r v-diffusion-pytorch/diffusion pixray/.")
         print("installing git dependencies")
         os.system(f"pip install -r {requirementPath}")
 
