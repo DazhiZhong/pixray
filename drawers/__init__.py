8120b42fc7c30156b850411e8f569b4ba8dad3e1
Dazhi Zhong <dazhizho@usc.edu>
2022-01-07 16:33:05 -0800

    0

--- drawers/__init__.py
+++ drawers/__init__.py
@@ -1,7 +1,16 @@
 from .vqgan import VqganDrawer
 from .vdiff import VdiffDrawer
 from .fftdrawer import FftDrawer
-from .clipdrawer import ClipDrawer
-from .pixeldrawer import PixelDrawer
-from .dotdrawer import DotDrawer
-from .linedrawer import LineDrawer
+import_diffvg_drawers = False
+try:
+    import pydiffvg
+    import diffvg
+except Exception as e:
+    import_diffvg_drawers = True
+if import_diffvg_drawers:
+    from .clipdrawer import ClipDrawer
+    from .pixeldrawer import PixelDrawer
+    from .dotdrawer import DotDrawer
+    from .linedrawer import LineDrawer
+else:
+    print("no diffvg support")
